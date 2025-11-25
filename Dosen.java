b245429485e3f00b5c69f9ec8eea9083c1ad993e
shfawr <shifaalyadewi7@gmail.com>
2025-11-25 10:47:30 +0700

    Create Dosen class with constructor and getters

---
+++ Dosen.java
@@ -0,0 +1,17 @@
+public class Dosen {
+    private String nama;
+    private String nip;
+
+    public Dosen(String nama, String nip) {
+        this.nama = nama;
+        this.nip = nip;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getNip() {
+        return nip;
+    }
+}
